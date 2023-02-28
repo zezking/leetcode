4baee1b49960f865bbc6221f73dfd0d033c97b56
Enze Zhao <zhaoenze001@gmail.com>
2023-02-28 12:07:13 -0800

    Add test cases
    Implement better solution
    
    Format comment

--- running-sum-of-1d-array/runningSumOf1DArray.java
+++ running-sum-of-1d-array/runningSumOf1DArray.java
@@ -1,3 +1,4 @@
+
 /* Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 Return the running sum of nums.
 example 1:
@@ -14,18 +15,53 @@ example 3:
 
 input: nums = [3,1,2,10,1]
 Output: [3,4,6,16,17] */
- class Solution {
-        public int[] runningSum(int[] nums) {
-                int[] result;
-                int sum;
-                result = new int[nums.length];
-                for (int i = 0; i < nums.length; i++) {
-                        sum = nums[i];
-                        for (int j = 0; j < i; j++) {
-                                sum += nums[j];
-                        }
-                                result[i] = sum;
+// class Solution {
+//         public int[] runningSum(int[] nums) {
+//                 int[] result;
+//                 int sum;
+//                 result = new int[nums.length];
+//                 for (int i = 0; i < nums.length; i++) {
+//                         sum = nums[i];
+//                         for (int j = 0; j < i; j++) {
+//                                 sum += nums[j];
+//                         }
+//                         result[i] = sum;
+//                 }
+//                 return result;
+//         }
+// }
+import java.util.Arrays;
+
+public class Main {
+        // static int[] runningSum(int[] nums) {
+        // int[] result;
+        // int sum;
+        // result = new int[nums.length];
+        // for (int i = 0; i < nums.length; i++) {
+        // sum = nums[i];
+        // for (int j = 0; j < i; j++) {
+        // sum += nums[j];
+        // }
+        // result[i] = sum;
+        // }
+        // return result;
+        // };
+
+        // Better solution
+        static int[] runningSum(int[] nums) {
+                int[] result = new int[nums.length];
+                result[0] = nums[0];
+                for (int index = 1; index < nums.length; index++) {
+
+                        result[index] = result[index - 1] + nums[index];
                 }
                 return result;
         }
+
+        public static void main(String[] args) {
+
+                int[] nums = { 3, 1, 2, 10, 1 };
+                System.out.println(Arrays.toString(runningSum(nums)));
+        };
+
 }
