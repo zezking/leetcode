1b3d436b6d4fe2bd5e1d707747fb86a4d8cc6e42
Enze Zhao <zhaoenze001@gmail.com>
2023-03-03 20:51:10 -0800

    Isomorphic String WIP

---
+++ isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,31 @@
+public class Main {
+        static boolean isIsomorphic(String s, String t) {
+
+                String patternS;
+                String patternT;
+                for(int i=0; i<s.length; i++){
+                        
+                }
+        }
+
+        // Solution 2
+        static boolean isIsomorphic2(String s, String t) {
+
+        }
+
+        public static void main(String[] args) {
+                String s = "egg" ;
+                String t = "add"; 
+                String s2 = "foo"; 
+                String t2 = "bar"; 
+                String s3 = "paper"; 
+                String t3 = "title"; 
+                System.out.println(isIsomorphic(s,t));
+                System.out.println(isIsomorphic(s2,t2));
+                System.out.println(isIsomorphic(s3,t3));
+                System.out.println(isIsomorphic2(s,t));
+                System.out.println(isIsomorphic2(s2,t2));
+                System.out.println(isIsomorphic2(s3,t3));
+        };
+
+}
