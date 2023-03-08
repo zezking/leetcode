d5c0bc4e8a07883af3e6d78c25bf303dda3b0034
Enze Zhao <zhaoenze001@gmail.com>
2023-03-07 19:25:21 -0800

    create is subsequence directory
    Set up is sequence

---
+++ is-subsequence/is-subsequence.java
@@ -0,0 +1,23 @@
+public class Main {
+        static boolean isSubsequence(String s, String t) {
+
+                }
+        }
+
+        // Solution 2
+        static boolean isSubsequence(String s, String t) {
+
+        }
+
+        public static void main(String[] args) {
+                String s = "abc" ;
+                String t = "ahbgdc"; 
+                String s2 = "axc"; 
+                String t2 = "ahbgdc"; 
+                System.out.println(isSubsequence(s,t));
+                System.out.println(isSubsequence(s2,t2));
+                System.out.println(isSubsequence(s,t));
+                System.out.println(isSubsequence(s2,t2));
+        };
+
+}
