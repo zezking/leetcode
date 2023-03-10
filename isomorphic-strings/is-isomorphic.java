a280a03c14c9a47904acf11f8ef1d159c9a25dfd
Enze Zhao <zhaoenze001@gmail.com>
2023-03-10 12:11:28 -0800

    Add example solution to is-isomorphic question

--- isomorphic-strings/is-isomorphic.java
+++ isomorphic-strings/is-isomorphic.java
@@ -1,11 +1,13 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Arrays;
 
 public class Main {
         static boolean isIsomorphic(String s, String t) {
 
                 Map<String, String> hashMapStoT = new HashMap<String, String>();
                 Map<String, String> hashMapTtoS = new HashMap<String, String>();
+
                 for (int i = 0; i < s.length(); i++) {
                         String cS = String.valueOf(s.charAt(i));
                         String cT = String.valueOf(t.charAt(i));
@@ -22,12 +24,38 @@ public class Main {
 
                 }
                 return true;
-        }
+    }
 
         // Solution 2
-        // static boolean isIsomorphic2(String s, String t) {
-        //
-        // }
+        static boolean isIsomorphic2(String s, String t) {
+
+                int[] mappingDictStoT = new int[256];
+                Arrays.fill(mappingDictStoT, -1);
+
+                int[] mappingDictTtoS = new int[256];
+                Arrays.fill(mappingDictTtoS, -1);
+
+                for (int i = 0; i < s.length(); ++i) {
+                        char c1 = s.charAt(i);
+                        char c2 = t.charAt(i);
+
+                        //Use ASCII code to create mapping
+                        // Case 1: No mapping exists in either of the dictionaries
+                        if (mappingDictStoT[c1] == -1 && mappingDictTtoS[c2] == -1) {
+                                mappingDictStoT[c1] = c2;
+                                mappingDictTtoS[c2] = c1;
+                        }
+
+                        // Case 2: Ether mapping doesn't exist in one of the dictionaries or Mapping
+                        // exists and
+                        // it doesn't match in either of the dictionaries or both
+                        else if (!(mappingDictStoT[c1] == c2 && mappingDictTtoS[c2] == c1)) {
+                                return false;
+                        }
+                }
+
+                return true;
+        }
 
         public static void main(String[] args) {
                 String s = "egg";
@@ -36,12 +64,16 @@ public class Main {
                 String t2 = "bar";
                 String s3 = "paper";
                 String t3 = "title";
+                String s4 = "babc";
+                String t4 = "baba";
                 System.out.println(isIsomorphic(s, t));
                 System.out.println(isIsomorphic(s2, t2));
                 System.out.println(isIsomorphic(s3, t3));
-                // System.out.println(isIsomorphic2(s, t));
-                // System.out.println(isIsomorphic2(s2, t2));
-                // System.out.println(isIsomorphic2(s3, t3));
+                System.out.println(isIsomorphic(s4, t4));
+                System.out.println(isIsomorphic2(s, t));
+                System.out.println(isIsomorphic2(s2, t2));
+                System.out.println(isIsomorphic2(s3, t3));
+                System.out.println(isIsomorphic2(s4, t4));
         };
 
 }
