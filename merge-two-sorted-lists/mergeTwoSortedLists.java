bcf1d4417789416baf1bf7d83747a2255b800a4a
Enze Zhao <zhaoenze001@gmail.com>
2023-03-11 15:06:16 -0800

    Add recursion solution

--- merge-two-sorted-lists/mergeTwoSortedLists.java
+++ merge-two-sorted-lists/mergeTwoSortedLists.java
@@ -1,3 +1,30 @@
+import java.util.Objects;
+import java.util.function.Predicate;
+
+public class Main {
+        static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+                if (list1 == null) {
+                        return list2;
+                }
+                if (list2 == null) {
+                        return list1;
+                }
+                if (list1.val < list2.val) {
+                        list1.next = mergeTwoLists(list1.next, list2);
+                        return list1;
+                } else {
+                        list2.next = mergeTwoLists(list1, list2.next);
+                        return list2;
+                }
+        }
+
+        public static void main(String[] args) {
+                ListNode list1 = ListNode.createList(1, 2, 4);
+                ListNode list2 = ListNode.createList(1, 3, 4);
+                System.out.print(mergeTwoLists(list1, list2).toString());
+        };
+}
+
 public class ListNode {
         int val;
         ListNode next;
@@ -46,17 +73,4 @@ public class ListNode {
         public int hashCode() {
                 return Objects.hash(val, next);
         }
-}
-
-public class Main {
-        static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-
-        }
-
-        public static void main(String[] args) {
-                ListNode list1 = ListNode.createList(1, 2, 4);
-                ListNode list2 = ListNode.createList(1, 3, 4);
-                ListNode result = mergeTwoLists(list1, list2);
-        };
-
 }
