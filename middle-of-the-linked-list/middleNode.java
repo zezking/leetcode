6536908252fc5106b606df871c668aa368ce7167
Enze Zhao <zhaoenze001@gmail.com>
2023-03-19 16:19:15 -0700

    Set up middle of the linked list

---
+++ middle-of-the-linked-list/middleNode.java
@@ -0,0 +1,65 @@
+
+import java.util.Objects;
+import java.util.function.Predicate;
+
+public class Main {
+        static ListNode middleNode(ListNode head) {
+        }
+
+        public static void main(String[] args) {
+                ListNode list1 = ListNode.createList(1, 2, 3, 4, 5);
+                ListNode list2 = ListNode.createList(1, 2, 3, 4, 5, 6);
+                System.out.println(middleNode(list1).toString());
+                System.out.println(middleNode(list2).toString());
+        };
+}
+
+public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+                val = x;
+        }
+
+        public static ListNode createList(int... values) {
+                ListNode head = new ListNode(values[0]);
+                ListNode prev = head;
+                for (int i = 1; i < values.length; i++) {
+                        prev.next = new ListNode(values[i]);
+                        prev = prev.next;
+                }
+                return head;
+        }
+
+        public ListNode filter(Predicate<ListNode> f) {
+
+                ListNode node = this;
+                while (node != null && !f.test(node)) {
+                        node = node.next;
+                }
+                return node;
+        }
+
+        @Override
+        public String toString() {
+
+                return val + "->" + next;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+                if (this == o)
+                        return true;
+                if (o == null || getClass() != o.getClass())
+                        return false;
+                ListNode listNode = (ListNode) o;
+                return val == listNode.val &&
+                                Objects.equals(next, listNode.next);
+        }
+
+        @Override
+        public int hashCode() {
+                return Objects.hash(val, next);
+        }
+}
