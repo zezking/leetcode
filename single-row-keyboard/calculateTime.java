29243a6d00957190e646d23700528794303e8c5b
Enze Zhao <zhaoenze001@gmail.com>
2023-03-31 09:58:06 -0700

    Set up single row keyboard question

---
+++ single-row-keyboard/calculateTime.java
@@ -0,0 +1,14 @@
+public class Main {
+
+        public static void main(String[] args) {
+
+                string keyboard1 = "abcdefghijklmnopqrstuvwxyz", word1 = "cba";
+                string keyboard2 = "pqrstuvwxyzabcdefghijklmno", word2 = "leetcode";
+                System.out.println(Arrays.toString(calculateTime(nums)));
+        }
+
+        static int[] calculateTime(String keyboard, String word) {
+
+        };
+
+}
