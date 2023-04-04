9de191ace2be094ef08bcfd9226c1a1b81668818
Enze Zhao <zhaoenze001@gmail.com>
2023-04-04 13:09:31 -0700

    Set up binary search problem

---
+++ binary-search/search.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+        public static void main(String[] args) {
+
+                int[] num1 = { -1, 0, 3, 5, 9, 12 };
+                int target1 = 9;
+                int[] num2 = { -1, 0, 3, 5, 9, 12 };
+                int target2 = 2;
+                System.out.println(search1(keyboard1, word1));
+                System.out.println(search1(keyboard2, word2));
+        }
+
+        static int search1(String keyboard, String word) {
+        };
+
+        static int search2(String keyboard, String word) {
+        };
+}
