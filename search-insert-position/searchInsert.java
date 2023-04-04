8945abdde3c7972a43ce4c32a7d7ae65207b9950
Enze Zhao <zhaoenze001@gmail.com>
2023-04-04 16:32:01 -0700

    Set up searchInsert solution

---
+++ search-insert-position/searchInsert.java
@@ -0,0 +1,19 @@
+public class Main {
+
+        public static void main(String[] args) {
+
+                int[] num1 = {1,3,5,6};
+                int target1 = 5;
+                int[] num2 = {1,3,5,6};
+                int target2 = 2;
+                int[] num3 = {1,3,5,6};
+                int target3 = 7;
+                System.out.println(searchInsert(num1, target1));
+                System.out.println(searchInsert(num2, target2));
+                System.out.println(searchInsert(num3, target3));
+        }
+
+        static int searchInsert(int[] nums, int target) {
+        };
+
+}
