9056af5eabb0c816dc670ad1896dce361cf4fe03
Enze Zhao <zhaoenze001@gmail.com>
2023-04-10 22:05:59 -0700

    Add binary search approch to search insert problem

--- search-insert-position/searchInsert.java
+++ search-insert-position/searchInsert.java
@@ -2,11 +2,11 @@ public class Main {
 
         public static void main(String[] args) {
 
-                int[] num1 = {1,3,5,6};
+                int[] num1 = { 1, 3, 5, 6 };
                 int target1 = 5;
-                int[] num2 = {1,3,5,6};
+                int[] num2 = { 1, 3, 5, 6 };
                 int target2 = 2;
-                int[] num3 = {1,3,5,6};
+                int[] num3 = { 1, 3, 5, 6 };
                 int target3 = 7;
                 System.out.println(searchInsert(num1, target1));
                 System.out.println(searchInsert(num2, target2));
@@ -14,6 +14,20 @@ public class Main {
         }
 
         static int searchInsert(int[] nums, int target) {
+                int left=0;
+                int right=nums.length-1;
+
+                while(left<=right){
+                        int pivot=left+(right-left)/2;
+                        if(nums[pivot]==target){
+                                return pivot;
+                        } else if(nums[pivot]<target){
+                                left=pivot+1;
+                        } else {
+                                right=pivot-1;
+                        }
+                }
+                return left;
         };
 
 }
