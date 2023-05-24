0143d31fd9f38f83110d29465c5a055b673daa85
Enze Zhao <zhaoenze001@gmail.com>
2023-05-24 11:34:06 -0700

    Add bucket sort solution to top k frequent problems

--- neetcode/top-k-frequent-elements/top-k-frequent.ts
+++ neetcode/top-k-frequent-elements/top-k-frequent.ts
@@ -15,3 +15,34 @@ function topKFrequent(nums: number[], k: number): number[] {
 
   return result;
 }
+
+//Bucket sort solution
+function topKFrequent2(nums: number[], k: number): number[] {
+  const count = new Array<number[]>(nums.length + 1);
+  const group = new Map<number, number>();
+  const result: number[] = [];
+
+  for (const num of nums) {
+    if (!group.has(num)) {
+      group.set(num, 1);
+    } else {
+      group.set(num, group.get(num)! + 1);
+    }
+  }
+
+  for (let i = 0; i < count.length; i++) {
+    count[i] = [];
+  }
+
+  for (const [key, counts] of group) {
+    count[counts].push(key);
+  }
+
+  for (let i = count.length - 1; i > -1; i--) {
+    if (count[i].length !== 0 && k > 0) {
+      result.push(...count[i]);
+      k--;
+    }
+  }
+  return result;
+}
