1306254e9022a38638e0a5e92ff2e0512068ef6f
Enze Zhao <zhaoenze001@gmail.com>
2024-07-28 16:47:48 -0700

    Add solution to merge two sorted linkLists

---
+++ neetcode/merge-two-sorted-lists/merge-two-sorted-lists.ts
@@ -0,0 +1,30 @@
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val = 0, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+function mergeTwoLists(list1, list2) {
+    const dummyNode = new ListNode()
+    let node: ListNode = dummyNode
+
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            node.next = list1
+            list1 = list1.next
+        } else {
+            node.next = list2
+            list2 = list2.next
+        }
+        node = node.next as ListNode
+    }
+    if (list1) {
+        node.next = list1
+    } else {
+        node.next = list2
+    }
+    return dummyNode.next
+}
