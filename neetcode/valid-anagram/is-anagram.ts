6e5740b8a74c3d278bcae9a8a2d79769c2509dc2
Enze Zhao <zhaoenze001@gmail.com>
2024-07-28 21:53:58 -0700

    Add new solution to isAnagram

--- neetcode/valid-anagram/is-anagram.ts
+++ neetcode/valid-anagram/is-anagram.ts
@@ -17,3 +17,32 @@ function isAnagram(s: string, t: string): boolean {
 
   return true;
 }
+
+
+/*
+Time O(n)
+Space O(n)
+*/
+
+function isAnagram2(s, t) {
+  if (s.length !== t.length) {
+    return false
+  }
+  const sHashMap = {}
+  const tHashMap = {}
+  for (let i = 0; i < s.length; i++) {
+    sHashMap[s[i]] = sHashMap[s[i]] ? sHashMap[s[i]] + 1 : 1
+    tHashMap[t[i]] = tHashMap[t[i]] ? tHashMap[t[i]] + 1 : 1
+  }
+
+  for (const c in sHashMap) {
+    if (sHashMap[c] !== tHashMap[c]) {
+      return false
+    }
+  }
+  return true
+}
+
+function isAnagram3(s, t) {
+  return s.sort() !== t.sort()
+}
